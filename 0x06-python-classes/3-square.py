e7d47acd92e03e40727f3870c0aede31d40cdbe8
maina-al3x <alexwachira4@gmail.com>
2023-03-28 13:31:52 -0700

    Task3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Area of a square
+"""
+
+
+class Square:
+    """define variables and methods"""
+
+    def __init__(self, size=0):
+        """initialize attributes"""
+        if isinstance(size, int) and size >= 0:
+            self.__size = size
+        elif not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """define area method, evaluate square area"""
+        return self.__size ** 2
