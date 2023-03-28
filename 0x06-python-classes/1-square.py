e90e53b72a52c6628602ce8867bb8ae95ca7aa91
maina-al3x <alexwachira4@gmail.com>
2023-03-28 13:22:05 -0700

    Task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Square with size
+"""
+
+
+class Square:
+    """define variables and methods"""
+
+    def __init__(self, size=0):
+        """initialize attribute"""
+        self.__size = size
